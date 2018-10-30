5bacefe5a26d6ef30aaa5732da7271157140351c
Alex Friedman <alexfriedman115@cs.uchicago.edu>
2018-10-30 11:44:07 -0500

    started stuff

--- src/edu/virginia/lab2test/LabTwoGame.java
+++ src/edu/virginia/lab2test/LabTwoGame.java
@@ -1,7 +1,10 @@
 package edu.virginia.lab2test;
 
 import edu.virginia.engine.display.AnimatedSprite;
+import edu.virginia.engine.display.DisplayObject;
+import edu.virginia.engine.display.DisplayObjectContainer;
 import edu.virginia.engine.display.Game;
+import edu.virginia.engine.display.Sprite;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -16,13 +19,15 @@ public class LabTwoGame extends Game{
 
 	static /* Create a sprite object for our game. We'll use mario */
 	AnimatedSprite mario = new AnimatedSprite("Mario", "Mario.png", new Point(0,0));
+	static DisplayObjectContainer sun = new DisplayObjectContainer("sun", "sun.png");
+	static DisplayObject earth = new DisplayObject("earth", "earth.png");
 
 
 	/**
 	 * Constructor. See constructor in Game.java for details on the parameters given
 	 * */
 	public LabTwoGame() {
-		super("Lab Two Test Game", 1000, 300);
+		super("Lab Two Test Game", 500, 500);
 	}
 	
 	/**
@@ -32,102 +37,15 @@ public class LabTwoGame extends Game{
 	@Override
 	public void update(ArrayList<Integer> pressedKeys){
 		super.update(pressedKeys);
-		mario.setPlaying(false);
-
-		if(mario.isJumping()){
-			if(mario.getJumpTimer()>4){
-				mario.setPosition(new Point(mario.getPosition().x , mario.getPosition().y-12));
-				mario.setJumpTimer(mario.getJumpTimer()-1);
-			}else if (mario.getJumpTimer()>0) {
-				mario.setPosition(new Point(mario.getPosition().x, mario.getPosition().y + 12));
-				mario.setJumpTimer(mario.getJumpTimer() - 1);
-			}else{
-				mario.setJumping(false);
-			}
-
-		}else{
-
-			/* Make sure mario is not null. Sometimes Swing can auto cause an extra frame to go before everything is initialized */
-			if (mario != null) mario.update(pressedKeys);
-
-
-			/* Make sure mario is not null. Sometimes Swing can auto cause an extra frame to go before everything is initialized */
-			if (mario != null) mario.update(pressedKeys);
-
-			//Move
-			if (pressedKeys.contains(KeyEvent.VK_UP)) {
-				mario.setJumping(true);
-				//mario.setPlaying(true);
-				mario.setPosition(new Point(mario.getPosition().x, mario.getPosition().y - 12));
-				if(mario.getFacing()==1){
-					//mario.animate(4,5);
-				}else if(mario.getFacing()==-1){
-					//mario.animate(6,7);
-				}
-				mario.setJumpTimer(7);
-			}
-			if (pressedKeys.contains(KeyEvent.VK_RIGHT)) {
-				mario.animate(0, 1);
-				mario.setPlaying(true);
-				mario.setFacing(1);
-				mario.setPosition(new Point(mario.getPosition().x + 5, mario.getPosition().y));
-			}
-			if (pressedKeys.contains(KeyEvent.VK_LEFT)) {
-				mario.animate(2, 3);
-				mario.setPlaying(true);
-				mario.setFacing(-1);
-				mario.setPosition(new Point(mario.getPosition().x - 5, mario.getPosition().y));
-			}
-
-			//Animation Speed
-			if (pressedKeys.contains(KeyEvent.VK_X)) {
-				mario.setAnimationSpeed(mario.getAnimationSpeed() + 5);
-				if (mario.getAnimationSpeed() > 350) {
-					mario.setAnimationSpeed(350);
-				}
-			}
-			if (pressedKeys.contains(KeyEvent.VK_Z)) {
-				mario.setAnimationSpeed(mario.getAnimationSpeed() - 5);
-				if (mario.getAnimationSpeed() < 25) {
-					mario.setAnimationSpeed(25);
-				}
-			}
-			if (pressedKeys.contains(KeyEvent.VK_C)) {
-				System.out.println("Animation Speed: " + mario.getAnimationSpeed());
-			}
-
-			if (pressedKeys.contains(null)) {
-				if (mario.getFacing() == 1) {
-					mario.animate(0, 0);
-				} else if (mario.getFacing() == -1) {
-					mario.animate(2, 2);
-				}
-			}
-
-			//scale
-			if (pressedKeys.contains(KeyEvent.VK_A)) {
-				mario.setScaleX(mario.getScaleX() + .05);
-				mario.setScaleY(mario.getScaleY() + .05);
-			}
-			if (pressedKeys.contains(KeyEvent.VK_S)) {
-				mario.setScaleX(mario.getScaleX() - .05);
-				mario.setScaleY(mario.getScaleY() - .05);
-				if ((mario.getScaleX() < 0) || (mario.getScaleY() < 0)) {
-					mario.setScaleX(0);
-					mario.setScaleY(0);
-				}
-			}
-		}
-
 		
-		if((mario.getGameClock().getElapsedTime()>mario.getAnimationSpeed())&&(mario.isPlaying())){
-			if (mario.getCurrentFrame() < mario.getEndFrame())
-				mario.setCurrentFrame(mario.getCurrentFrame() + 1);
-			else 
-				mario.setCurrentFrame(mario.getStartFrame());
-			
-        	mario.getGameClock().resetGameClock();
-		}
+		if (pressedKeys.contains(KeyEvent.VK_UP)) 
+			sun.setPosition(new Point(sun.getPosition().x, sun.getPosition().y + 5));
+		if (pressedKeys.contains(KeyEvent.VK_DOWN)) 
+			sun.setPosition(new Point(sun.getPosition().x, sun.getPosition().y - 5));
+		if (pressedKeys.contains(KeyEvent.VK_RIGHT)) 
+			sun.setPosition(new Point(sun.getPosition().x - 5, sun.getPosition().y));
+		if (pressedKeys.contains(KeyEvent.VK_LEFT)) 
+			sun.setPosition(new Point(sun.getPosition().x + 5, sun.getPosition().y));
 		
 	}
 	
@@ -139,8 +57,8 @@ public class LabTwoGame extends Game{
 	public void draw(Graphics g){
 		super.draw(g);
 		
-		/* Same, just check for null in case a frame gets thrown in before Mario is initialized */
-		if(mario != null) mario.draw(g);
+		/* Same, just check for null in case a frame gets thrown in before Mario is initialized */		
+		if (sun != null) sun.draw(g);
 	}
 
 	/**
@@ -150,13 +68,13 @@ public class LabTwoGame extends Game{
 	 * */
 	public static void main(String[] args) throws IOException {
 		LabTwoGame game = new LabTwoGame();
-
-		mario.setPosition(new Point(mario.getPosition().x + 5, mario.getPosition().y+50));
-
-		mario.populate();
-		mario.animate(0,1);
-		mario.setAnimationSpeed(100);
+		sun.setScaleX(.5);
+		sun.setScaleY(.5);
+		sun.setPosition(new Point(180, 180));
 		
+//		earth.setScaleX(.2);
+//		earth.setScaleY(.2);
+		sun.addChild(earth);
 		
 		game.start();
 	}
